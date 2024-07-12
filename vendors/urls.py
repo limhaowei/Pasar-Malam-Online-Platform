395396ccbe8e9f14723da20789559a70f7986c86
Venus <venus.ty.wong@gmail.com>
2024-07-13 01:48:22 +1000

    Added templates, static files and amended views and url py files.

--- vendors/urls.py
+++ vendors/urls.py
@@ -1,12 +1,13 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_vendor, name = "register_vendor"),
-    path('', views.homepage, name='homepage'),
-    path('vendor/<pk>/', views.vendor_page, name='vendor_page'),
-    
+    path("", views.homepage, name="homepage"),
+    path("register/", views.register_vendor, name="register_vendor"),
+    path("vendors/", views.vendors_page, name="vendor_list"),
+    path("vendor/<pk>/", views.vendor_detail, name="vendor_detail"),
+    path("login/", views.login, name="login"),
+    path("user-guide/", views.user_guide, name="user_guide"),
+    path("apply-slot/", views.apply_slot, name="apply_slot"),
+    path("blog/", views.blog, name="blog"),
 ]
-
-
--- vendors/views.py
+++ vendors/views.py
@@ -1,31 +1,65 @@
-
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Vendor
 from .forms import VendorForm
 
 
-
-
+# register-vendor.html / register_vendor.html template
 def register_vendor(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = VendorForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('homepage')
+            return redirect("homepage")
     else:
         form = VendorForm()
-    return render(request, 'register_vendor.html', {'form': form})
+    return render(request, "register_vendor.html", {"form": form})
 
 
+# login.html
+def login(request):
+    return render(request, "login.html")
+
+
+# index.html
 def homepage(request):
+    selected_vendor = Vendor.objects.all().first()
+    vendors = Vendor.objects.all()[:5]
+    context = {
+        "vendors": vendors,
+        "selected_vendor": selected_vendor,
+    }
+    return render(request, "index.html", context)
+
+
+# vendors.html / vendor_list.html template
+def vendors_page(request):
     vendors = Vendor.objects.all()
-    return render(request, 'vendor_list.html', {'vendors': vendors})
+    return render(request, "vendor_list.html", {"vendors": vendors})
 
 
-def vendor_page(request, pk):
+# detail.html
+def vendor_detail(request, pk):
     vendor = get_object_or_404(Vendor, pk=pk)
-    return render(request, 'vendor_page.html', {'vendor': vendor})
+    return render(request, "vendor_page.html", {"vendor": vendor})
 
 
+# user_guide.html
+def user_guide(request):
+    return render(request, "user_guide.html")
 
+
+# apply_slot.html
+def apply_slot(request):
+    return render(request, "apply_slot.html")
+
+
+# vendor of the week
+def blog(request):
+    selected_vendor = Vendor.objects.all().first()
+    context = {
+        "selected_vendor": selected_vendor,
+        "title": "Vendor of the Week",
+        "content": "This is the content of the blog",
+    }
+    return render(request, "blog.html", context)
