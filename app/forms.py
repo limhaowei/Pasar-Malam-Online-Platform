d0257d8fc6404942d55a95b49abf67119ee9e2e0
Hao Wei Lim <94880011+limhaowei@users.noreply.github.com>
2024-07-12 19:16:21 +0800

    removed because corruption

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Vendor
-
-
-class VendorForm(forms.ModelForm):
-    class Meta:
-        model = Vendor
-        fields= ('name', 'certificate', 'social_media_alias', 'owner')
